bdac0b63b52bc5d246cfddc66c68b55f2d24dbc6
Neven Gluhovic <nevengluhovic@gmail.com>
2021-12-11 13:10:27 +0100

    Moving everything to CoinList

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import Coin from "./Coin";
-import NoResult from "./NoResult";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const CoinList = ({ coins, setCoins }) => {
   const [search, setSearch] = useState("");
@@ -16,7 +17,10 @@ const CoinList = ({ coins, setCoins }) => {
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <h1 className="coin-text">Search Currency</h1>
+        <h1 className="coin-text">
+          Search Currency{" "}
+          <FontAwesomeIcon icon={faSearch} className="result-icon" />
+        </h1>
         <form action="">
           <input
             onChange={inputChangeHandler}
@@ -26,13 +30,44 @@ const CoinList = ({ coins, setCoins }) => {
           />
         </form>
       </div>
-      {filteredCoins.length === 0 ? <NoResult /> : ""}
-      {filteredCoins.map((coin) => (
-        <Coin key={coin.id} coin={coin} />
-      ))}
-      {/* {filteredCoins.map((filter) => {
-        <Coin coins={coins} />;
-      })} */}
+      <div
+        className={`no-result ${filteredCoins.length !== 0 ? "hidden" : ""}`}
+      >
+        <h1>No Results Found</h1>
+        <p>We couldn't find anything! :(</p>
+      </div>
+
+      <table>
+        <thead className={filteredCoins.length === 0 ? "hidden" : ""}>
+          <tr>
+            <th>Name</th>
+            <th>Code</th>
+            <th>Price</th>
+            <th>24h Change</th>
+            <th>Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredCoins.map((coin) => (
+            <tr>
+              <td>
+                <img src={coin.image} alt="crypto" />
+                {coin.id}
+              </td>
+              <td>{coin.symbol.toUpperCase()}</td>
+              <td>${coin.current_price.toLocaleString()}</td>
+              <td
+                className={` ${
+                  coin.price_change_percentage_24h > 0 ? "green" : "red"
+                }`}
+              >
+                {coin.price_change_percentage_24h.toFixed(2)}%
+              </td>
+              <td>${coin.market_cap.toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
